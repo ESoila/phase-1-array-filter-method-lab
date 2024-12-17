2265f466c80c5cc498c487b06c6917b2d122537b
soila <elizabeth.soila@student.moringaschool.com>
2024-12-17 11:19:55 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+// Function 1: Case insensitive match
+function findMatching(drivers, query) {
+    return drivers.filter((driver) => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // Function 2: Match names that start with a specific string
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter((driver) => driver.startsWith(query));
+  }
+  
+  // Function 3: Match driver objects with a specific name
+  function matchName(drivers, query) {
+    return drivers.filter((driver) => driver.name === query);
+  }
+  
